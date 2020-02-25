9a2631d4904e22a893b094ca79324c9a67fc637e
Walter Bacchetti <wb@magma.consulting>
2020-02-25 14:02:32 +0100

    Added Dates Lib (for standard and Italian dateTimeNow in Europe/Rome TimeZone)

---
+++ Libs/Dates.php
@@ -0,0 +1,23 @@
+<?php
+namespace \Magma\Util\Libs;
+
+class Dates
+{
+    protected function getDatetimeNow()
+    {
+        $data = new \DateTime('now', new \DateTimeZone("Europe/Rome"));
+        return $data;
+    }
+
+    public function getDateTimeNowStandard()
+    {
+        $data = $this->getDatetimeNow();
+        return $data->format('Y-m-d H:i:s');
+    }
+
+    public function getDateTimeNowItalian()
+    {
+        $data = $this->getDatetimeNow();
+        return $data->format('d/m/Y H:i:s');
+    }
+}
